b7280c3ed4cf2574176fb89077963acbc330abd6
Gayoung03 <gayoungk0312@gmail.com>
2026-03-20 17:28:42 +0900

    [Bronze I] Title: 암호 키, Time: 1436 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1816. 암호 키/암호 키.py
+++ Python/백준/Bronze/1816. 암호 키/암호 키.py
@@ -1,12 +1,11 @@
-N = int(input())
+S = int(input())
 
-for _ in range(N):
-    s = int(input())
-
-    for i in range(2,1000000+1):
-        if s % i == 0:
+for _ in range(S):
+    num = int(input())
+    
+    for j in range(2,1000000+1):
+        if num % j == 0:
             print("NO")
             break
-        if i == 1000000: # i가 10^6까지 됐다는 건 그 전에 인수가 없었다는 거니까
+        if j == 1000000:
             print("YES")
-    
