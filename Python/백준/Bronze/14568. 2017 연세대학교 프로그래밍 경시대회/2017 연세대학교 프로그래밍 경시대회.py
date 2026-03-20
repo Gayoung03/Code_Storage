82fc7669360f8c7e3d70b3669cef2ce9913d73db
Gayoung03 <gayoungk0312@gmail.com>
2026-03-20 17:35:47 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ Python/백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -1,17 +1,20 @@
 N = int(input())
 result = 0
 '''
-a >= b+2
-c%2 !=0
-a>0, b>0, c>0
-'''
+a,b,c에 들어갈 수 있는 숫자는 각각 0부터 6임(조건을 무시하고)
+그래서 일단 반복문으로 다 연결함
 
-for A in range(N+1):
-    for B in range(N+1):
-        for C in range(N+1):
-            if A+B+C == N:
-                if A>=B+2:
-                    if A!=0 and B !=0 and C !=0:
-                        if C %2 ==0:
+각각의 조건을 나열함
+이걸 만족한다면? 그 다음에 이것도 만족한다면? 또 이것도 만족한다면? -> 원하는 조합이다!!
+result의 갯수 올리기
+'''
+for a in range(N+1):
+    for b in range(N+1):
+        for c in range(N+1):
+            if a+b+c == N:
+                if a >= b+2:
+                    if a !=0 and b!=0 and c!=0:
+                        if c%2 ==0:
                             result+=1
+
 print(result)
